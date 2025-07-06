b41431e00df8e02b0fb44737b85ef71ba64cbca6
brunaldihno <111083391+brunaldihno@users.noreply.github.com>
2025-07-06 15:04:30 -0400

    Create example.py

---
+++ nodes/example.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class ExampleNode(Node):
+  
+  def __init__(self):
+    super().__init__('nodito')
+    self.pub = self.create_publisher(String, "/nodito_talker", 10)
+    self.timer = self.create_timer(1, self.talk)
+
+  def talk(self):
+    msg = Srting()
+    msg.data = "nodito is alive"
+    self.pub.publish(msg)
+
+
+def main():
+  rclpy.init()
+  node = ExampleNode()
+  rclpy.spin(node)
+  node.destroy_node()
+  rclpy.shutdown()
+
+
+if __name__ == '__main__':
+  main()
+
+
+
